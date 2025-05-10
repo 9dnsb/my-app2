721ee94366db2f0d817cb23b1afdb6fd8b73f3e8
David Blatt <davidblatt10@gmail.com>
2025-05-10 00:13:35 -0400

    added user forms

--- src/lib/getUsersByType.ts
+++
@@ -1,9 +0,0 @@
-import { UserType } from '@/app/generated/prisma'
-import prisma from '@/lib/prisma'
-
-export async function getUsersByType(type: UserType) {
-  return await prisma.user.findMany({
-    where: { type },
-    orderBy: { id: 'asc' },
-  })
-}
